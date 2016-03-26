c7d86b815b0e17b8a4864fe1c3f2ef358ea4ae56
Michael Liang <michaelpliang@gmail.com>
2016-03-26 11:33:05 -0700

    implemented doubly linked list and tests

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -4,3 +4,29 @@ export function DoublyLinkedList() {
   this.length = 0;
   this.head = null;
 }
+
+DoublyLinkedList.prototype = {
+  push: function (val) {
+    var head = this.head,
+      current = head,
+      previous = head;
+    if (!head) {
+      this.head = {
+        value: val,
+        previous: null,
+        next: null
+      };
+    } else {
+      while (current && current.next) {
+        previous = current;
+        current = current.next;
+      }
+      current.next = {
+        value: val,
+        previous: current,
+        next: null
+      }
+    }
+    this.length++;
+  }
+}
--- test/DoublyLinkedList_spec.js
+++ test/DoublyLinkedList_spec.js
@@ -8,12 +8,29 @@ describe('Doubly Linked List', () => {
 
 
   describe('Constructor', () => {
-
+    let x = new DoublyLinkedList();
+    it('Should should create a new linked list with length of 0', () => {
+      expect(x).to.have.ownProperty('length');
+      expect(x).to.have.lengthOf(0);
+    });
+    it('Should have no value at head', () => {
+      expect(x).to.have.property('head');
+      expect(x.head).to.equal(null);
+    });
   });
 
 
   describe('Add', () => {
-
+    let x = new DoublyLinkedList();
+    it('Should add new values to list', () => {
+      x.push(1);
+      x.push(2);
+      x.push(3);
+      expect(x).to.have.lengthOf(3);
+      expect(x.head.value).to.equal(1);
+      expect(x.head.next.value).to.equal(2);
+      expect(x.head.next.next.value).to.equal(3);
+    });
   });
 
   describe('Remove', () => {
