c2aedabf3268944c1641b80c06480f97a8fdcb94
Michael Liang <michaelpliang@gmail.com>
2016-03-26 11:07:27 -0700

    singly linked lists with tests

---
+++ SinglyLinkedList.js
@@ -0,0 +1,52 @@
+'use strict';
+
+export function LinkedList() {
+  this.length = 0;
+  this.head = null;
+}
+
+LinkedList.prototype = {
+
+  push: function (val) {
+    let node = {
+      value: val,
+      next: null
+    };
+
+    if (!this.head) {
+      this.head = node;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.length++;
+  },
+
+  remove: function (val) {
+    let current = this.head;
+
+    //Value you want to remove is at the head, replace with next node
+    if (current.value === val) this.head = current.next;
+    //Value you want to remove is in the middle
+    else {
+      let previous = current;
+      while (current.next) {
+        if (current.value === val) {
+          previous.next = current.next;
+          break;
+        }
+        previous = current;
+        current = current.next;
+        // console.log(previous, current);
+      }
+      //Value you want to replace is at the end, remove and set next value to null
+      if (current.value === val) previous.next = null;
+    }
+    this.length--;
+    // console.log(this.head);
+  }
+
+}
---
+++ test/SinglyLinkedList_spec.js
@@ -0,0 +1,54 @@
+'use strict';
+
+import {expect} from 'chai';
+import {LinkedList} from '../SinglyLinkedList';
+
+
+describe('Singly Linked List', () => {
+
+
+  describe('Constructor', () => {
+    let x = new LinkedList();
+    it('Should should create a new linked list with length of 0', () => {
+      expect(x).to.have.ownProperty('length');
+      expect(x).to.have.lengthOf(0);
+    });
+    it('Should have no value at head', () => {
+      expect(x).to.have.property('head');
+      expect(x.head).to.equal(null);
+    });
+  });
+
+
+  describe('Add', () => {
+    let x = new LinkedList();
+    it('Should add new values to list', () => {
+      x.push(1);
+      x.push(2);
+      x.push(3);
+      expect(x).to.have.lengthOf(3);
+      expect(x.head.value).to.equal(1);
+      expect(x.head.next.value).to.equal(2);
+      expect(x.head.next.next.value).to.equal(3);
+    });
+  });
+
+  describe('Remove', () => {
+    let x = new LinkedList();
+    x.push(1);
+    x.push(2);
+    x.push(3);
+    it('Should remove values from list', () => {
+      x.remove(1);
+      expect(x).to.have.lengthOf(2);
+      expect(x.head.value).to.equal(2);
+      expect(x.head.next.value).to.equal(3);
+      x.push(4);
+      x.remove(3);
+      // console.log(x.head);
+      expect(x).to.have.lengthOf(2);
+      expect(x.head.value).to.equal(2);
+      expect(x.head.next.value).to.equal(4);
+    });
+  });
+});
