0029d67ab30caa39a3fb176742c4af6cf94cc00e
Michael Liang <michaelpliang@gmail.com>
2016-03-26 11:32:43 -0700

    implemented circular linked list and test

--- CircularLinkedList.js
+++ CircularLinkedList.js
@@ -4,3 +4,32 @@ export function CircularLinkedList() {
   this.length = 0;
   this.head = null;
 }
+
+CircularLinkedList.prototype = {
+  push: function (val) {
+    var head = this.head,
+      current = head,
+      node = {
+        value: val,
+        next: null,
+        previous: null
+      };
+
+    if (!head) {
+      node.next = node;
+      node.previous = node;
+      this.head = node;
+    } else {
+      while (current.next != head) {
+        current = current.next;
+      }
+
+      node.next = head;
+      node.previous = current;
+
+      head.previous = node;
+      current.next = node;
+    }
+    length++;
+  }
+}
